2c9c553df0c089897e44e1d6ee136b68ad298419
Shane Holloway <125281980+shanewholloway-usds-2@users.noreply.github.com>
2024-04-03 23:01:58 -0400

    Refactored uuid-lookup model to support FAFSA UUIDs with different semantics: unaffected, potentially affected, unknown

--- fafsa-uuid/js/uuid-lookup.js
+++ fafsa-uuid/js/uuid-lookup.js
@@ -2,11 +2,7 @@ import {aiter_stream_lines} from "https://cdn.jsdelivr.net/npm/csv-iter-parse@0.
 import {imm, imm_set, imm_html, imm_raf} from 'https://cdn.jsdelivr.net/npm/imm-dom@0.3.7/esm/index.min.js'
 import {create_uuid_trie} from './uuid-trie.js'
 
-
-window.on_use_csv_file = async (file) =>
-    await on_load_csv_of_uuids( aiter_stream_lines( file.stream() ) )
-
-export const uuid_search_api = {
+const fafsa_uuid_model = {
     history: new Map(),
 
     search(uuid_search) {
@@ -25,20 +21,40 @@ export const uuid_search_api = {
         window.search_result_tbody.prepend(el_search_result)
     },
 
+    _detail_by_semantic: {
+        potentially_affected: {
+            label: 'potentially affected applications',
+            render_match: el_status =>
+                imm_set(el_status, {class:'search-result-potentially-affected'}, 'Potentially affected'),
+            render_absent: el_status =>
+                imm_set(el_status, {class:'search-result-unaffected'}, 'Unaffected'),
+        },
+        unaffected: {
+            label: 'unaffected applications',
+            render_match: el_status =>
+                imm_set(el_status, {class:'search-result-unaffected'}, 'Unaffected'),
+            render_absent: el_status =>
+                imm_set(el_status, {class:'search-result-potentially-affected'}, 'Potentially affected'),
+        },
+        neutral: {
+            label: 'unknown semantics',
+            render_match: el_status =>
+                imm_set(el_status, {class:'search-result-match'}, 'Present'),
+            render_absent: el_status =>
+                imm_set(el_status, {class:'search-result-nomatch'}, imm_html.b('Not'), ' present'),
+        }
+    },
     async _update_status(uuid_search, el_status) {
-        do {
-            let match_found = window.uuid_trie.lookup(uuid_search)
+        for(; true; await imm_raf()) {
+            let match_found = this.uuid_trie.lookup(uuid_search)
 
-            if (match_found) {
-                return imm_set(el_status, {class:'hint-match'}, imm_html.b('Present'))
-            } else if ('ready' == document.documentElement.dataset.status) {
-                return imm_set(el_status, {class:'hint-nomatch'}, imm_html.b('Not'), ' present')
-            } else if (!el_status.className) {
+            if (match_found)
+                return this._semantic.render_match(el_status)
+            else if ('ready' == document.documentElement.dataset.status)
+                return this._semantic.render_absent(el_status)
+            else if (!el_status.className)
                 imm_set(el_status, {class:'hint-caution'}, imm_html.i('(searching)'))
-            }
-
-            await imm_raf()
-        } while (1)
+        }
     },
 
     clear() {
@@ -63,50 +79,76 @@ export const uuid_search_api = {
     evt_input(evt) {
         return this.search(evt.target.value)
     },
-}
-window.uuid_search_api = uuid_search_api
 
+    async evt_load_csv_of_fafsa_uuids(file) {
+        return await this.load_csv_of_fafsa_uuids(
+            aiter_stream_lines( file.stream() ) )
+    },
 
+    async load_csv_of_fafsa_uuids(aiter_lines) {
+        this.clear()
+        let status_target = document.documentElement.dataset
+        try {
+            // Determine FAFSA UUID semantic from CSV header line
+            let {value: ln_header} = await aiter_lines.next()
+            console.log('CSV header: %o')
+            this._determine_csv_semantic(ln_header)
 
-const _csv_uuid_progress = {
-    n_report: 5000,
-    update(ts0) {
-        let td = (Date.now() - ts0)/1000
-        let el = window.progress_tgt
-        el.querySelector('[name=uuid_count]').textContent = `${uuid_trie.n}`
+            this.uuid_trie = window.uuid_trie = create_uuid_trie()
 
-        //let [[,rec]] = uuid_trie.shared.max.node.db
-        //console.log('Sample UUID:', rec.uuid)
-        return imm_raf()
-    }
-}
+            let ts0 = new Date()
+            status_target.status = 'loading'
+            for await (let n of this._aiter_load_csv_of_uuids(aiter_lines))
+                await this._load_progress_update(ts0, uuid_trie)
+            await this._load_progress_update(ts0, uuid_trie)
 
-export async function on_load_csv_of_uuids(aiter_lines, progress=_csv_uuid_progress) {
-    document.documentElement.dataset.status = 'loading'
-
-    let ts0 = new Date()
-    window.uuid_trie = create_uuid_trie()
-    for await (let n of _aiter_load_csv_of_uuids(window.uuid_trie, aiter_lines, progress.n_report))
-        await progress.update(ts0)
-    await progress.update(ts0)
-
-    document.documentElement.dataset.status = 'ready'
-    return window.uuid_trie
-}
-async function * _aiter_load_csv_of_uuids(uuid_trie, aiter_lines, n_batch) {
-    // skip CSV header line
-    await aiter_lines.next()
-
-    let n = n_batch ?? 5000
-    for await (let ln of aiter_lines) {
-        let uuid = ln.split(',')[0].replaceAll('"', '')
-        uuid_trie.add_record({uuid})
-
-        if (0 >= --n) {
-            // yield to UI thread after a batch
-            yield n = n_batch
+            status_target.status = 'ready'
+            return uuid_trie
+        } catch (err) {
+            status_target.status = 'error'
+            throw err
         }
-    }
+    },
 
-    console.log('Done loading UUIDs', {uuid_trie})
+    _determine_csv_semantic(ln_header) {
+        if (ln_header.match(/^(FAFSA_UUID_POTENTIALLY_AFFECTED|FAFSA_UUID_AFFECTED)\b/i)) {
+            this.semantic = 'potentially_affected'
+            this._semantic = this._detail_by_semantic.potentially_affected
+        } else if (ln_header.match(/^(FAFSA_UUID_UNAFFECTED|FAFSA_UUID)\b/i)) {
+            this.semantic = 'unaffected'
+            this._semantic = this._detail_by_semantic.unaffected
+        } else {
+            this.semantic = 'neutral'
+            this._semantic = this._detail_by_semantic.neutral
+        }
+        window.progress_tgt.querySelector('[name=semantic]').textContent = `${this._semantic.label}`
+        window.search_result_tbody.parentElement.classList.add('semantic-'+this.semantic)
+        console.log('FAFSA UUID semantic: %o', this.semantic)
+        return this.semantic
+    },
+
+    async _load_progress_update(ts0, uuid_trie) {
+        let td = (Date.now() - ts0)/1000
+        window.progress_tgt.querySelector('[name=uuid_count]').textContent = `${uuid_trie.n}`
+        await imm_raf()
+    },
+
+    _n_report_batch: 5000,
+    async * _aiter_load_csv_of_uuids(aiter_lines) {
+        let n_batch = this._n_report_batch
+        let n = n_batch ?? 5000
+        let uuid_trie = this.uuid_trie
+        for await (let ln of aiter_lines) {
+            let uuid = ln.split(',')[0].replaceAll('"', '')
+            uuid_trie.add_record({uuid})
+
+            if (0 >= --n) {
+                // yield to UI thread after a batch
+                yield n = n_batch
+            }
+        }
+
+        console.log('Done loading FAFSA UUIDs', uuid_trie.shared, {uuid_trie}, )
+    }
 }
+window.fafsa_uuid_model = fafsa_uuid_model
